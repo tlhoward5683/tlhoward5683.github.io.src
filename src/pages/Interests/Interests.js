67acf8598109f5452275ac9dc5c8fa3ad116f6ba
Tiffany Howard <tiffanyl.howard@gmail.com>
2021-12-15 13:43:36 -0600

    Updated Interests

--- src/pages/Interests/Interests.js
+++ src/pages/Interests/Interests.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const Interests = () => {
     return (
@@ -6,7 +6,7 @@ const Interests = () => {
             <h1 className="text-center page-header">
                 Interests
             </h1>
-            
+
             Believe it or not, I don't spend all of my time coding.  I know, shocking!<br /><br />
             
             I grew up in Austin, Texas, and I love all of the outdoorsy stuff the area has to offer from hiking the 
@@ -15,7 +15,7 @@ const Interests = () => {
             I have a soft spot for animals, whether they be neighborhood stray cats or squirrels.  If it's hungry, Imma 
             feed it.<br /><br />
 
-            I also enjoy some flower gardening now and then...when it's not too hot or too cold out.<br /><br />
+            I also enjoy some flower gardening now and then...when it's not too hot or too cold out.  Austin is great for that!<br /><br />
 
             So there ya go.  That about sums me up!
         </div>
